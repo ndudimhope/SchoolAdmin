81cbe9528088e8a3991a36d5e68aabda2bf7b3a6
Hope Ndudim <59125945+ndudimhope@users.noreply.github.com>
2021-08-16 10:26:41 +0100

    consumed teacher service in program.cs

---
+++ Ado.Net-Demo/SqlDataService/TeacherService.cs
@@ -0,0 +1,124 @@
+﻿using SchoolAdmin.Ado.Net_Demo.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin.Ado.Net_Demo.SqlDataService
+{
+    class TeacherService
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter adp;
+        SqlDataReader rdr;
+
+        public TeacherService()
+        {
+            conn = new SqlConnection("Data Source=.;Initial Catalog=SchoolAdminDB;Integrated Security=True;Pooling=False");
+        }
+
+        public void Insert(TeacherDTO dataToInsert)
+        {
+            string commandStr = $"INSERT INTO Teachers(FirstName, MiddleName, LastName, Subject) " +
+                $"VALUES('{dataToInsert.FirstName}', '{dataToInsert.MiddleName}', '{dataToInsert.LastName}', '{dataToInsert.Subject}')";
+            cmd = new SqlCommand(commandStr, conn);
+            conn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            Console.WriteLine($"Successfully inserted {rowsAffected} records into the 'Teachers' table.");
+        }
+
+
+        public List<TeacherDTO> FetchAll()
+        {
+            List<TeacherDTO> result = new List<TeacherDTO>();
+            string commandStr = "SELECT * FROM Teachers";
+            cmd = new SqlCommand(commandStr, conn);
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                result.Add(new TeacherDTO
+                {
+                    StaffId = (int)rdr["StaffId"],
+                    FirstName = (string)rdr["FirstName"],
+                    MiddleName = rdr["MiddleName"] == DBNull.Value ? string.Empty : (string)rdr["MiddleName"],
+                    LastName = (string)rdr["LastName"],
+                    Subject = (string)rdr["Subject"]
+                }); ;
+            }
+            conn.Close();
+            return result;
+        }
+
+
+        public List<TeacherDTO> FetchWithFilter(KeyValuePair<string, object> filterPair, string comparer)
+        {
+            List<TeacherDTO> result = new List<TeacherDTO>();
+
+            // SQL query without parameters
+            string commandStr = "SELECT * FROM Teachers WHERE " + $"{filterPair.Key} {comparer} '{filterPair.Value}'";
+            //string commandStr3 = "SELECT * FROM Teachers WHERE Subject = 'Physics' OR Subject = 'Chemistry' OR Subject = 'Biology'";
+
+            // SQL query with parameters
+            // string commandStr2 = "SELECT * FROM Teachers WHERE " + $"{filterPair.Key} {comparer} @param";
+            //string commandStr4 = "SELECT * FROM Teachers WHERE Subject = @p1 OR Subject = @p2 OR Subject = @p3";
+
+            cmd = new SqlCommand(commandStr, conn);
+            //// Specify parameters here, if any
+            //cmd.Parameters.AddWithValue("param", filterPair.Value);
+            //cmd.Parameters.AddWithValue("p1", "Physics");
+            //cmd.Parameters.AddWithValue("p2", "Chemistry");
+            //cmd.Parameters.AddWithValue("p3", "Biology");
+
+            conn.Open();
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                result.Add(new TeacherDTO
+                {
+                    StaffId = (int)rdr["StaffId"],
+                    FirstName = (string)rdr["FirstName"],
+                    MiddleName = rdr["MiddleName"] == DBNull.Value ? string.Empty : (string)rdr["MiddleName"],
+                    LastName = (string)rdr["LastName"],
+                    Subject = (string)rdr["Subject"]
+                }); ;
+            }
+            conn.Close();
+            return result;
+        }
+
+
+        public void Update(KeyValuePair<string, object> filterPair, string comparer, TeacherDTO newData)
+        {
+            string filterStr = " WHERE " + $"{filterPair.Key} {comparer} '{filterPair.Value}'";
+
+            string updateStr = newData.FirstName == null ? "" : $" FirstName = '{newData.FirstName}',";
+            updateStr += newData.MiddleName == null ? "" : $" MiddleName = '{newData.MiddleName}',";
+            updateStr += newData.LastName == null ? "" : $" LastName = '{newData.LastName}',";
+            updateStr += newData.Subject == null ? "" : $" Subject = '{newData.Subject}',";
+
+            string commandStr = $"UPDATE Teachers SET " + updateStr.TrimEnd(',') + filterStr;
+            cmd = new SqlCommand(commandStr, conn);
+            conn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            Console.WriteLine($"Successfully updated {rowsAffected} records in the 'Teachers' table.");
+        }
+
+
+        public void Delete(KeyValuePair<string, object> filterPair, string comparer)
+        {
+            string commandStr = $"DELETE FROM Teachers WHERE {filterPair.Key} {comparer} '{filterPair.Value}'";
+            cmd = new SqlCommand(commandStr, conn);
+            conn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            Console.WriteLine($"Successfully deleted {rowsAffected} records from the 'Teachers' table.");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SchoolAdmin.Ado.Net_Demo.DTO;
+using SchoolAdmin.Ado.Net_Demo.SqlDataService;
+using System;
 
 namespace SchoolAdmin
 {
@@ -6,7 +8,18 @@ namespace SchoolAdmin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Instantiate the TeacherService class
+            TeacherService teacherService = new TeacherService();
+
+            // Define and insert Teacher objects
+            TeacherDTO teacher1 = new TeacherDTO() { FirstName = "Mustapha", MiddleName = "", LastName = "Rufai", Subject = "JavaScript" };
+            TeacherDTO teacher2 = new TeacherDTO() { FirstName = "Frank", MiddleName = "", LastName = "Legborsi", Subject = "DevOps" };
+            TeacherDTO teacher3 = new TeacherDTO() { FirstName = "John", MiddleName = "Tony", LastName = "Ubi", Subject = "Product Design" };
+            TeacherDTO teacher4 = new TeacherDTO() { FirstName = "Tonia", MiddleName = "Seyi", LastName = "Adegbite", Subject = "Python" };
+            teacherService.Insert(teacher1);
+            teacherService.Insert(teacher2);
+            teacherService.Insert(teacher3);
+            teacherService.Insert(teacher4);
         }
     }
 }
